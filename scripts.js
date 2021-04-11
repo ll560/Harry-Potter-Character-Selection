f696e803bd4375b91135c0af810dfefaf91593ab
ll560 <llavia6@gmail.com>
2021-04-10 23:54:26 -0400

    update to js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
-const addTitle = () =>{
+const addTitle = () => { //why is this whole function showing in the console log
     //add a function to find h1
+    title = document.querySelector("h1")
+    title = document.createElement("h1")
+    tile.innerText = "Harry Potter Character Selection";
+    title.append(title.innerText)
+    return addTitle;
+} 
+
+const addTitle2 = () => {
+    document.getElementById('h1').innerHTML = "Harry Potter Character Selection";
+}
+//tried to solve this 2 ways. One of them worked.....which one?
+
+
+
+// Create a function called flexIt(), which should find the first element with the class called 'needFlex' and add the class flexIt to that element and then toggle the flexIt class on and off each time that function is called by the user clicking the button that says "Flex houses".
+
+
+const flexIt = () => {
+    firstElement = document.querySelector("needFlex");
+    
 } 
