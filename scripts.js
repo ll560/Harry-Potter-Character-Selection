0b1f3ab5d94639e5adcfdf1e390b9e193a295fcc
ll560 <llavia6@gmail.com>
2021-04-12 16:50:57 -0400

    update js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,11 @@
 const addTitle = () => { //why is this whole function showing in the console log
-    //add a function to find h1
-    title = document.querySelector("h1")
-    title = document.createElement("h1")
-    tile.innerText = "Harry Potter Character Selection";
-    title.append(title.innerText)
-    return addTitle;
+    
+    let title = document.querySelector("h1")
+    title.textContent= "Harry Potter Character Selection"
 } 
+ addTitle();
 
-const addTitle2 = () => {
-    document.getElementById('h1').innerHTML = "Harry Potter Character Selection";
-}
-//tried to solve this 2 ways. One of them worked.....which one?
+
 
 
 
@@ -18,6 +13,31 @@ const addTitle2 = () => {
 
 
 const flexIt = () => {
-    firstElement = document.querySelector("needFlex");
-    
+    let allElements = document.getElementsByClassName("needFlex");
+    allElements[0].classList.toggle("flexIt");
 } 
+flexIt();
+
+
+
+
+
+// Create a function called addHouses(), which will find all the paragraphs that have a class called "description" and will set (replace) another class name. The new class name should be the name of the Hogwarts house the text is describing. Example: If the paragraph is about Ravenclaw, find that paragraph and set the class attribute to include ravenclaw as the 2nd class.
+
+// const addHouses = () => {
+//     oldElement='description'
+//     newClassName='p';
+//    const oldElement = document.getElementsByClassName("description"); //get the classname and turns it to array
+//    const houses = document.getElementsByClassName("houses")//selects the parent of the element to be replaced
+//     houses.replaceChild(newClassName, oldElement);
+// }
+
+// document.querySelectorAll('.description') //Do I need to include the dot here or is that only in the console?
+
+// Create a function called emphasize() with one parameter called house. When the user clicks the button labeled with one of the four Hogwarts houses, your function applies the CSS styling associated with the .emphasis class to both the paragraph that describes that house and the people listed below that belong to that house. Example: When you have the HTML file open in the browser, if the user clicks the Gryffindor button at the top, the description of the Gryffindor house and the people in Gryffindor from the list at the bottom will all have blue backgrounds and yellow borders around them. When the user clicks that Gryffindor button again, the styling should disappear. 
+
+const emphasize = (house) => {
+    let gryffindor_div= document.querySelector(".house");
+    gryffindor_div.querySelector(".description").style.backgroundColor = "blue"; //is that the correct way to add background color?
+    return emphasize;
+}
